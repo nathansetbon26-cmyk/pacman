9fe74f40a5d14eaa6a996d7be632bb82ab0c22c5
Natan <nathansetbon26@gmail.com>
2026-02-23 14:50:28 +0200

    fixed

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ class Coin(arcade.Sprite):
         self.center_y = y
         self.value = value
 
-
 class Character(arcade.Sprite):
     def __init__(self, started_x, started_y, speed, color):
         super().__init__()
@@ -33,35 +32,85 @@ class Character(arcade.Sprite):
         self.change_x = 0
         self.change_y = 0
 
-
 class Player(Character):
     def __init__(self, started_x, started_y):
         super().__init__(started_x, started_y, 0.125, arcade.color.YELLOW)
         self.score = 0
         self.lives = 3
+        self.angle = 0
+        self.mouth_open = True
+        self.mouth_timer = 0
 
     def move(self):
         self.center_x += self.change_x * self.speed
         self.center_y += self.change_y * self.speed
+        if self.change_x > 0:
+            self.angle = 0
+        elif self.change_x < 0:
+            self.angle = 180
+        elif self.change_y > 0:
+            self.angle = 90
+        elif self.change_y < 0:
+            self.angle = 270
+
+    def update_animation(self, delta_time):
+        self.mouth_timer += delta_time
+        if self.mouth_timer > 0.15:
+            self.mouth_open = not self.mouth_open
+            self.mouth_timer = 0
+
+    def draw(self):
+        radius = (TILE_SIZE-6) * 0.5
+        start_angle = 0
+        end_angle = 360
+        if self.mouth_open:
+            start_angle = 30
+            end_angle = 330
+        arcade.draw_arc_filled(self.center_x, self.center_y,
+                               radius*2, radius*2,
+                               arcade.color.YELLOW,
+                               start_angle + self.angle,
+                               end_angle + self.angle)
 
 class Enemy(Character):
-    def __init__(self, started_x, started_y):
-        super().__init__(started_x, started_y, 2.5, arcade.color.RED)
+    def __init__(self, started_x, started_y, color=arcade.color.RED):
+        super().__init__(started_x, started_y, 4, color)
         self.time_to_change_direction = 0
+        self.target_x = self.center_x
+        self.target_y = self.center_y
+        self.color = color
 
     def pick_new_direction(self):
         movements = [(0, 1), (0, -1), (1, 0), (-1, 0), (0, 0)]
         fate = random.choice(movements)
         self.change_x = fate[0]
         self.change_y = fate[1]
-        self.time_to_change_direction = random.uniform(0.3, 1.0)
+        self.time_to_change_direction = random.uniform(0.1, 1.0)
+        self.target_x = self.center_x + self.change_x * TILE_SIZE
+        self.target_y = self.center_y + self.change_y * TILE_SIZE
 
     def update(self, delta_time=1/60):
         if self.time_to_change_direction <= 0:
             self.pick_new_direction()
-        self.center_x += self.change_x * self.speed
-        self.center_y += self.change_y * self.speed
+        self.center_x += (self.target_x - self.center_x) * 0.1
+        self.center_y += (self.target_y - self.center_y) * 0.1
         self.time_to_change_direction -= delta_time
+
+    def draw(self):
+        x = self.center_x
+        y = self.center_y
+        width = TILE_SIZE - 8
+        height = TILE_SIZE - 8
+
+        # גוף מלא חצי עיגול למעלה
+        arcade.draw_ellipse_filled(x, y, width, height, self.color)
+
+
+        eye_radius = 3
+        arcade.draw_circle_filled(x - width*0.2, y + height*0.15, eye_radius, arcade.color.WHITE)
+        arcade.draw_circle_filled(x + width*0.2, y + height*0.15, eye_radius, arcade.color.WHITE)
+        arcade.draw_circle_filled(x - width*0.2, y + height*0.15, eye_radius-1, arcade.color.BLUE)
+        arcade.draw_circle_filled(x + width*0.2, y + height*0.15, eye_radius-1, arcade.color.BLUE)
 
 
 class Wall(arcade.Sprite):
@@ -73,8 +122,6 @@ class Wall(arcade.Sprite):
         self.height = texture.height
         self.center_x = center_x
         self.center_y = center_y
-
-
 
 class PacmanGame(arcade.View):
     def __init__(self):
@@ -93,14 +140,13 @@ class PacmanGame(arcade.View):
         self.clear()
         self.wall_list.draw()
         self.coin_list.draw()
-        self.ghost_list.draw()
-        self.player_list.draw()
+        for ghost in self.ghost_list:
+            ghost.draw()
+        self.player.draw()
         arcade.draw_text(f"Score: {self.player.score}",50,550,arcade.color.WHITE)
         arcade.draw_text(f"Lives: {self.player.lives}",50,500, arcade.color.WHITE)
         if self.game_over:
             arcade.draw_text("GAME OVER", 200, 300, arcade.color.GOLD, 60)
-
-
 
     def setup(self):
         self.wall_list = arcade.SpriteList()
@@ -108,38 +154,31 @@ class PacmanGame(arcade.View):
         self.ghost_list = arcade.SpriteList()
         self.player_list = arcade.SpriteList()
         self.game_over = False
-
         for row_idx, row in enumerate(LEVEL_MAP):
             for col_idx, cell in enumerate(row):
                 x = col_idx * TILE_SIZE + TILE_SIZE // 2
                 y = row_idx * TILE_SIZE + TILE_SIZE // 2
-
-                if LEVEL_MAP[row_idx][col_idx] == "#":
+                if cell == "#":
                     self.wall_list.append(Wall(x, y))
-
-                elif LEVEL_MAP[row_idx][col_idx] == "P":
+                elif cell == "P":
                     self.player=Player(x, y)
                     self.start_x = x
                     self.start_y = y
                     self.player_list.append(self.player)
-
-                elif LEVEL_MAP[row_idx][col_idx] == ".":
+                elif cell == ".":
                     self.coin_list.append(Coin(x, y))
-
-                elif LEVEL_MAP[row_idx][col_idx] == "G":
+                elif cell == "G":
                     self.ghost_list.append(Enemy(x, y))
-
-
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.UP:
-            self.player.change_y = 32
+            self.player.change_y = TILE_SIZE
         if key == arcade.key.DOWN:
-            self.player.change_y = -32
+            self.player.change_y = -TILE_SIZE
         if key == arcade.key.LEFT:
-            self.player.change_x = -32
+            self.player.change_x = -TILE_SIZE
         if key == arcade.key.RIGHT:
-            self.player.change_x = 32
+            self.player.change_x = TILE_SIZE
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
@@ -152,11 +191,11 @@ class PacmanGame(arcade.View):
             now_x = self.player.center_x
             now_y = self.player.center_y
             self.player.move()
+            self.player.update_animation(delta_time)
             wall_player = arcade.check_for_collision_with_list(self.player, self.wall_list)
             if len(wall_player)>0:
                 self.player.center_x = now_x
                 self.player.center_y = now_y
-
             for ghost in self.ghost_list:
                 current_x = ghost.center_x
                 current_y = ghost.center_y
@@ -165,29 +204,23 @@ class PacmanGame(arcade.View):
                 if len(ghost_wall):
                     ghost.center_x = current_x
                     ghost.center_y = current_y
-
                 coin_check = arcade.check_for_collision_with_list(self.player, self.coin_list)
                 if len(coin_check)>0:
                     self.player.score+=10
                     self.coin_list.remove(coin_check[0])
-
                 ghost_player = arcade.check_for_collision_with_list(self.player, self.ghost_list)
                 if len(ghost_player):
                     self.player.lives-=1
                     self.player.center_x=self.start_x
-                    self.player.center_y = self.start_x
+                    self.player.center_y = self.start_y
                     if self.player.lives==0:
                         self.game_over = True
 
-
-
-
-# מפה לדוגמה: # = קיר, . = מטבע, P = פקמן, G = רוח, רווח = כלום
 LEVEL_MAP = [
     "#########################",
-    "#..................G....#",
-    "#.###.####.#..#####.###.#",
     "#.......................#",
+    "#.###.####.#..#####.###.#",
+    "#.............G.........#",
     "#.####.####.##.####.###.#",
     "#............##.........#",
     "#.########.####.#######.#",
@@ -195,14 +228,13 @@ LEVEL_MAP = [
     "#.#########.####.######.#",
     "#............##.........#",
     "#.####.#####.##.###.###.#",
-    "#.......................#",
+    "#.......G...............#",
     "#.####.#####.#..###.###.#",
     "#............#..........#",
     "#########################",
 ]
 
 def main():
-    """פונקציית main שמריצה את המשחק."""
     window = arcade.Window(WIDTH_WINDOW, HEIGHT_WINDOW, WINDOW_TITLE)
     game_view = PacmanGame()
     game_view.setup()
@@ -213,10 +245,3 @@ if __name__ == "__main__":
     main()
 
 
-# מפה לדוגמה: # = קיר, . = מטבע, P = פקמן, G = רוח, רווח = כלום
-LEVEL_MAP = [
-    "###########",
-    "#P....G...#",
-    "#.........#",
-    "###########",
-]
